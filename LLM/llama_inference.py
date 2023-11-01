12b93f11ee3be284597f8236a6957448251f19d8
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2023-11-01 12:41:51 +0000

    cleaned up the script

--- LLM/llama_inference.py
+++ LLM/llama_inference.py
@@ -11,9 +11,26 @@ import time
 import torch
 from transformers import LlamaTokenizer
 
-from model_utils import load_model, load_peft_model
-
 import json
+
+from peft import PeftModel
+from transformers import LlamaForCausalLM
+
+# Function to load the main model for text generation
+def load_model(model_name, quantization):
+    model = LlamaForCausalLM.from_pretrained(
+        model_name,
+        return_dict=True,
+        load_in_8bit=quantization,
+        device_map="auto",
+        low_cpu_mem_usage=True,
+    )
+    return model
+
+# Function to load the PeftModel for performance optimization
+def load_peft_model(model, peft_model):
+    peft_model = PeftModel.from_pretrained(model, peft_model)
+    return peft_model
 
 def my_open_w(fpath):
     if not os.path.exists(os.path.dirname(fpath)):
