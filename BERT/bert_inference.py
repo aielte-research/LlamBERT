41316e11d95c4c2fcd59e82bd2ae1530d940f380
muzsailajos <muzsailajos@protonmail.com>
2023-11-02 13:21:00 +0000

    bert inference upgrades validation set neptune ai

--- BERT/bert_inference.py
+++ BERT/bert_inference.py
@@ -9,6 +9,9 @@ import torch.optim as optim
 from tqdm import tqdm
 import numpy as np
 import argparse
+import neptune
+from dotenv import load_dotenv
+import os
 
 
 def load_data_from_json(path):
@@ -82,20 +85,22 @@ class BERT(nn.Module):
         return out
 
 
-def finetune(epochs, dataloader, model, loss_fn, optimizer):
-    model.train()
-    for epoch in range(epochs):
-
+def finetune(num_epochs, train_dataloader, val_dataloader, model, loss_fn, optimizer, run):
+    
+    for epoch in range(num_epochs):
+        
+        # train loop
+        model.train()
         train_correct = 0
         train_samples = 0
         train_batch_accuracies = []
         
-        loop=tqdm(enumerate(dataloader), leave=False, total=len(dataloader))
-        for batch, dl in loop:
-            ids = dl['ids']
-            token_type_ids = dl['token_type_ids']
-            mask = dl['mask']
-            label =dl['target']
+        loop=tqdm(enumerate(train_dataloader), leave=False, total=len(train_dataloader))
+        for _, data in loop:
+            ids = data['ids']
+            token_type_ids = data['token_type_ids']
+            mask = data['mask']
+            label = data['target']
             label = label.unsqueeze(1)
             
             optimizer.zero_grad()
@@ -117,19 +122,69 @@ def finetune(epochs, dataloader, model, loss_fn, optimizer):
             train_correct += num_correct
             num_samples = pred.shape[0]
             train_samples += num_samples
-            accuracy = num_correct/num_samples
-            train_batch_accuracies.append(accuracy)
+            train_accuracy = num_correct/num_samples
+            train_batch_accuracies.append(train_accuracy)
             
             # Show progress while training
-            loop.set_description(f'Epoch={epoch}/{epochs}')
-            loop.set_postfix(loss=loss.item(),acc=accuracy)
+            loop.set_description(f'Epoch={epoch}/{num_epochs}')
+            loop.set_postfix(loss=loss.item(),acc=train_accuracy)
+            if run is not None:
+                run["train/loss"].append(loss.item())
+                run["train/accuracy"].append(train_accuracy)
             
-        print(f'Got {train_correct} / {train_samples} with accuracy {float(train_correct)/float(train_samples)*100:.2f}')
-        # plot train_batch_accuracies
+        print(f'Epoch={epoch}/{num_epochs}, Train:\t got {train_correct} / {train_samples} with accuracy {float(train_correct)/float(train_samples)*100:.2f}%')
+
+        with torch.no_grad():
+            model.eval()
+            val_correct = 0
+            val_samples = 0
+            
+            loop=tqdm(enumerate(val_dataloader), leave=False, total=len(train_dataloader))
+            for _, data in loop:
+                ids = data['ids']
+                token_type_ids = data['token_type_ids']
+                mask = data['mask']
+                label = data['target']
+                label = label.unsqueeze(1)
+                
+                
+                output=model(
+                    ids=ids,
+                    mask=mask,
+                    token_type_ids=token_type_ids)
+                label = label.type_as(output)
+
+                val_loss=loss_fn(output,label)
+                
+                pred = np.where(output >= 0, 1, 0)
+
+                num_correct = sum(1 for a, b in zip(pred, label) if a[0] == b[0])
+                val_correct += num_correct
+                num_samples = pred.shape[0]
+                val_samples += num_samples
+                val_accuracy = num_correct/num_samples
+                
+                # Show progress while training
+                loop.set_description(f'Epoch={epoch}/{num_epochs}')
+                loop.set_postfix(loss=val_loss.item(),acc=val_accuracy)
+                if run is not None:
+                    run["val/loss"].append(val_loss.item())
+                    run["val/accuracy"].append(val_accuracy)
+            
+        print(f'Epoch={epoch}/{num_epochs}, Validation:\t got {val_correct} / {val_samples} with accuracy {float(val_correct)/float(val_samples)*100:.2f}%')
+    
+    # plot train_batch_accuracies
+
     return model
 
 
 if __name__ == '__main__':
+    
+    load_dotenv()
+    run = neptune.init_run(
+        project="aielte/DNeurOn",
+        api_token=os.getenv("NEPTUNE_API_TOKEN"),
+    )
     parser = argparse.ArgumentParser(
                     prog='Bert finetuning',
                     description='This program finetunes a BERT model')
@@ -141,9 +196,10 @@ if __name__ == '__main__':
 
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-    dataset= BertDataset(tokenizer, max_length=100, data=data)
-
-    dataloader=DataLoader(dataset=dataset, batch_size=32, shuffle=True)
+    train_dataset= BertDataset(tokenizer, max_length=100, data=data[:-50])
+    train_dataloader=DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
+    val_dataset= BertDataset(tokenizer, max_length=100, data=data[-50:])
+    val_dataloader=DataLoader(dataset=val_dataset, batch_size=32, shuffle=False)
 
     model=BERT()
 
@@ -155,4 +211,12 @@ if __name__ == '__main__':
     for param in model.bert_model.parameters():
         param.requires_grad = False
 
-    model = finetune(5, dataloader, model, loss_fn, optimizer)
+    model = finetune(
+        num_epochs=5, 
+        train_dataloader=train_dataloader, 
+        val_dataloader=val_dataloader, 
+        model=model, 
+        loss_fn=loss_fn, 
+        optimizer=optimizer,
+        run=run
+    )
