6eb99c8a7666d6f44a814040239138596fcecda9
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2024-01-25 10:54:02 +0000

    Added Supergold data (commented out)

--- plots/makeplots_mislabel.py
+++ plots/makeplots_mislabel.py
@@ -4,7 +4,8 @@ import bokeh
 gp_params={
     "Ys": [
         [96.54, 96.50, 96.47, 96.35, 96.23, 95.90, 95.76, 94.82, 92.99, 75.67, 70.92, 66.86, 65.52, 47.13],
-        [76.27, 77.84, 78.93, 78.27, 78.14, 77.04, 74.29, 68.12, 60.36, 54.72, 54.16, 53.72, 53.57, 51.96]
+        [76.27, 77.84, 78.93, 78.27, 78.14, 77.04, 74.29, 68.12, 60.36, 54.72, 54.16, 53.72, 53.57, 51.96],
+        #[95.84, 95.70, 95.70, 95.75, 95.65, 95.46, 95.53, 94.74, 91.87, 60.36, 67.14, 52.14, 52.06, 49.42]
     ],
     "x": [0,1,2,5,10,20,30,40,45,48,48.5,48.8,49,50],
     "xlabel": "Mislabel %",
@@ -15,7 +16,7 @@ gp_params={
     "markers": None,
     "legend": {
         "location": "bottom_left",
-        "labels": ["finetuned","frozen"]#,"Our model with transformer encoder"]
+        "labels": ["finetuned","frozen"] #,"supergold"]
     },
     "matplotlib": {
         "calc_xtics": False,
@@ -24,7 +25,7 @@ gp_params={
         "style": "seaborn-poster",
         "png_dpi": 240
     },
-    "colors": [bokeh.palettes.Category10[10][0], bokeh.palettes.Category10[10][1]],
+    "colors": [bokeh.palettes.Category10[10][0], bokeh.palettes.Category10[10][1]], #, bokeh.palettes.Category10[10][2]],
     "dashes": ["solid","solid"],
     "line45_color": None,
     "baselines": {
