a78882a2fee30d662c406f0e3cd2a28bf5b2b27e
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2024-03-02 13:48:26 +0100

    Count all appearances

--- UMLS/filter_from_abstracts.py
+++ UMLS/filter_from_abstracts.py
@@ -11,6 +11,7 @@ sys.path.append('../')
 from utils import getTime, human_format, my_open
 import ahocorasick
 from tqdm import tqdm
+from collections import defaultdict 
 
 def filter_by_lang(lang,df):
     start_time = time()
@@ -39,28 +40,27 @@ def filter_by_kw_aho_(df,automaton,concepts,strict=True):
 
     return found,automaton
 
-def filter_by_kw_aho(df,automaton,concepts,strict=True):
-    found=[]
+def filter_by_kw_aho(df,automaton,concepts,syn2cui):
     for abst in tqdm(df['Abstract']):
-        found_cuis=[]
-        for end_index, (cui, original_value) in automaton.iter(abst):
-            if (not strict or re.search(r'\b' + re.escape(original_value) + r'\b',abst)) and not cui in found_cuis:
-                found_cuis.append(cui)
-                print(f"Found '{original_value}' (cui:{cui})")
+        found_cuis=set()
+        found_syns=set()
+        for _, (cui, syn) in automaton.iter(abst):
+            if re.search(r'\b' + re.escape(syn) + r'\b',abst) and not syn in found_syns:
+                for dup_cui in syn2cui[syn]:
+                    found_cuis.add(dup_cui)
+                    concepts[dup_cui]["synonyms"][syn]+=1
+                found_syns.add(syn)
         if len(found_cuis)>0:
             for cui in found_cuis:
-                for syn in concepts[cui]:
-                    automaton.remove_word(syn)
-            automaton.make_automaton()
-            found+=found_cuis
-            print(f"Found in chunk: {len(found)}")
+                concepts[cui]["count"]+=1
 
-    return found,automaton
+    return concepts,automaton
 
 def load_chunk(
         csv_path: str,
         automaton,
         concepts,
+        syn2cui,
         index: int,
         language: str="eng",
     ):
@@ -87,17 +87,27 @@ def load_chunk(
     # filter by concepts
     start_time = time()
     logger.info(f"Searching {len(concepts)} concepts in {human_format(len(df))} abstracts in chunk {index:02d}...")
-    found_cuis, automaton = filter_by_kw_aho(df, automaton, concepts)
-    logger.info(f"{len(found_cuis)} concepts found. {getTime(time() - start_time)}")
-    return found_cuis, automaton
+    concepts, automaton = filter_by_kw_aho(df, automaton, concepts, syn2cui)
+
+    return concepts, automaton
+
+def get_syn2cui(concepts):
+    ret = defaultdict(lambda:[])
+    for cui, syns in concepts.items():
+        for syn in syns:
+            ret[syn].append(cui)
+    return dict(ret)
 
 def main(
         csv_path: str="/home/projects/Brainvectors/PubMed/2024/pubmed_abstracts/",
         concepts_fpath: str="regions/region_concepts_combined-microsoft-BiomedBERT.json",
         output_path: str="regions",
         loglevel: int=20,
-        language: str="eng"
+        language: str="eng",
+        **kwargs
     ):
+    if len(kwargs) > 0:
+        raise ValueError(f"Unknown argument(s): {kwargs}")
     logger.setLevel(loglevel)
     streamHandler = logging.StreamHandler()
     logger.addHandler(streamHandler)
@@ -109,6 +119,20 @@ def main(
     # load concepts
     with open(concepts_fpath) as f:
         concepts = json.loads(f.read())
+
+    syn2cui = get_syn2cui(concepts)
+    output_fname = f'{os.path.basename(concepts_fpath).split(".")[0]}_duplicates.json'
+    with my_open(os.path.join(output_path,output_fname), 'w') as outfile:
+        json.dump({syn:cuis for syn,cuis in syn2cui.items() if len(cuis)>1}, outfile, indent=3)
+
+    automaton = ahocorasick.Automaton()    
+    for cui, entry in concepts.items():
+        for syn in entry:
+            if len(syn)>2:
+                automaton.add_word(syn, (cui, syn))    
+    automaton.make_automaton()
+
+    concepts = {cui: {"count":0,"synonyms":{s:0 for s in syns}} for cui, syns in concepts.items()}
     
     if os.path.isdir(csv_path):
         file_names = sorted(os.listdir(csv_path))
@@ -116,22 +140,14 @@ def main(
     else:
         csv_fpaths = [csv_path]
 
-    automaton = ahocorasick.Automaton()    
-    for cui, syns in concepts.items():
-        for syn in syns:
-            if len(syn)>2:
-                automaton.add_word(syn, (cui, syn))    
-    automaton.make_automaton()
-    found_cuis=[]
     for i, csv_fpath in enumerate(csv_fpaths):
-        found, automaton = load_chunk(csv_fpath, automaton, concepts, index=i+1, language=language)
-        found_cuis+=found
-
-    logger.info(f"{len(found_cuis)} concepts found out of {len(concepts)}.")
-
-    output_fname = f'{os.path.basename(concepts_fpath).split(".")[0]}_PubMed-filtered.json'
+        concepts, automaton = load_chunk(csv_fpath, automaton, concepts, syn2cui, index=i+1, language=language)
+    
+    concepts_res = {cui:entry for cui,entry in concepts.items() if entry["count"]>0}
+    logger.info(f"{len(concepts_res)} concepts kept out of {len(concepts)}.")
+    output_fname = f'{os.path.basename(concepts_fpath).split(".")[0]}_PubMed-filtered-counts.json'
     with my_open(os.path.join(output_path,output_fname), 'w') as outfile:
-        json.dump({cui:syns for cui,syns in concepts.items() if cui in found_cuis}, outfile, indent=3)
+        json.dump(concepts_res, outfile, indent=3)
 
     logger.info(f"Data prapared. Overall time: {getTime(time() - overall_start_time)}")
 
