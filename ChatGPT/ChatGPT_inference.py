6381f079be146a639fbe1a7130388439ee111af3
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2024-03-02 14:24:07 +0100

    change spacing

--- ChatGPT/ChatGPT_inference.py
+++ ChatGPT/ChatGPT_inference.py
@@ -11,105 +11,105 @@ sys.path.append("..")
 from utils import my_open, read_json_list
 
 def main(
-    prompt_file: str,
-    model_name: str="gpt-4",    
-    target_file: Union[str, None]=None,
-    **kwargs
+   prompt_file: str,
+   model_name: str="gpt-4",    
+   target_file: Union[str, None]=None,
+   **kwargs
 ):
-    if len(kwargs) > 0:
-        raise ValueError(f"Unknown argument(s): {kwargs}")
-    
-    args = dict(locals())
-    client = OpenAI()
-    
-    user_prompts = read_json_list(prompt_file)
+   if len(kwargs) > 0:
+      raise ValueError(f"Unknown argument(s): {kwargs}")
+   
+   args = dict(locals())
+   client = OpenAI()
+   
+   user_prompts = read_json_list(prompt_file)
 
-    if target_file is not None:
-        targets = read_json_list(target_file)
+   if target_file is not None:
+      targets = read_json_list(target_file)
 
-    questions=[]
-    answers=[]
-    answers_cleaned=[]
-    answers_binary=[]
-    # Process prompts in batches
-    for i in trange(len(user_prompts)):
-        if isinstance(user_prompts[i], list):
-            messages:list[dict] = user_prompts[i]
-        else:
-            messages=[
-                {"role": "user", "content": user_prompts[i]}
-            ]
+   questions=[]
+   answers=[]
+   answers_cleaned=[]
+   answers_binary=[]
+   # Process prompts in batches
+   for i in trange(len(user_prompts)):
+      if isinstance(user_prompts[i], list):
+         messages:list[dict] = user_prompts[i]
+      else:
+         messages=[
+            {"role": "user", "content": user_prompts[i]}
+         ]
 
-        get_answer = lambda messages: client.chat.completions.create(model=model_name, messages = messages, stream=False).choices[0].message.content
-        try:
+      get_answer = lambda messages: client.chat.completions.create(model=model_name, messages = messages, stream=False).choices[0].message.content
+      try:
+         answer = get_answer(messages)
+         if answer is None:
+            raise
+      except Exception as error:
+         print(error)
+         print("Inference failed, will try again in a minute...")
+         time.sleep(60)
+         try:
             answer = get_answer(messages)
             if answer is None:
-                raise
-        except Exception as error:
+               raise
+         except Exception as error:
             print(error)
-            print("Inference failed, will try again in a minute...")
-            time.sleep(60)
+            print("Inference failed, will try again in a 10 minutes...")
+            time.sleep(600)
             try:
-                answer = get_answer(messages)
-                if answer is None:
-                    raise
+               answer = get_answer(messages)
+               if answer is None:
+                  raise
             except Exception as error:
-                print(error)
-                print("Inference failed, will try again in a 10 minutes...")
-                time.sleep(600)
-                try:
-                    answer = get_answer(messages)
-                    if answer is None:
-                        raise
-                except Exception as error:
-                    print(error)
-                    print(f"Inference failed again, terminating session at prompt {i}...")
-                    break
-        #print(answer)
+               print(error)
+               print(f"Inference failed again, terminating session at prompt {i}...")
+               break
+      #print(answer)
 
-        questions.append(messages[-1]["content"])
-        answers.append(answer)
-        output_cleaned = answer.strip().strip(".,!?;'\"").strip().strip(".,!?;'\"").lower()
-        answers_cleaned.append(output_cleaned)
-        if output_cleaned == "no":
-            answers_binary.append(0)
-        elif output_cleaned == "yes":
-            answers_binary.append(1)
-        elif "negative" in output_cleaned and "positive" not in output_cleaned:
-            answers_binary.append(0)
-        elif "positive" in output_cleaned and "negative" not in output_cleaned:
-            answers_binary.append(1)
-        else:
-            answers_binary.append(2)
+      questions.append(messages[-1]["content"])
+      answers.append(answer)
+      output_cleaned = answer.strip().strip(".,!?;'\"").strip().strip(".,!?;'\"").lower()
+      answers_cleaned.append(output_cleaned)
+      if output_cleaned == "no":
+         answers_binary.append(0)
+      elif output_cleaned == "yes":
+         answers_binary.append(1)
+      elif "negative" in output_cleaned and "positive" not in output_cleaned:
+         answers_binary.append(0)
+      elif "positive" in output_cleaned and "negative" not in output_cleaned:
+         answers_binary.append(1)
+      else:
+         answers_binary.append(2)
 
-    output_data={
-        "settings": args,
-        "outputs": answers,
-        "outputs_cleaned": answers_cleaned,
-        "outputs_binary": answers_binary,
-        "output_stats": {
-            "negative": len([x for x in answers_binary if x==0]),
-            "positive": len([x for x in answers_binary if x==1]),
-            "other": len([x for x in answers_binary if x==2])
-        }
-    }
+   output_data={
+      "settings": args,
+      "outputs": answers,
+      "outputs_cleaned": answers_cleaned,
+      "outputs_binary": answers_binary,
+      "output_stats": {
+         "negative": len([x for x in answers_binary if x==0]),
+         "positive": len([x for x in answers_binary if x==1]),
+         "other": len([x for x in answers_binary if x==2])
+      }
+   }
 
-    if target_file is not None:        
-        correct=0
-        misclassifed=[]
-        for target, output, question in zip(targets[:len(answers_binary)], answers_binary, questions):
-            if target == output:
-                correct+=1
-            else:
-                misclassifed.append({"question":question,"answer": output})
-        output_data["accuracy"]=int(100000*correct/len(targets))/1000
-        print(f"Accuracy: {output_data['accuracy']}%")
-        output_data["misclassifed"]=misclassifed
-    
-    output_fpath = os.path.join("model_outputs",f'{os.path.splitext(os.path.basename(prompt_file))[0].strip(".")}_{os.path.basename(model_name.rstrip("/"))}.json')
-    with my_open(output_fpath) as file:
-        json.dump(output_data, file, indent=4)
+   if target_file is not None:        
+      correct=0
+      misclassifed=[]
+      for target, output, question in zip(targets[:len(answers_binary)], answers_binary, questions):
+         if target == output:
+            correct+=1
+         else:
+            misclassifed.append({"question":question,"answer": output})
+      output_data["accuracy"]=int(100000*correct/len(targets))/1000
+      print(f"Accuracy: {output_data['accuracy']}%")
+      output_data["misclassifed"]=misclassifed
+   
+   output_fpath = os.path.join("model_outputs",f'{os.path.splitext(os.path.basename(prompt_file))[0].strip(".")}_{os.path.basename(model_name.rstrip("/"))}.json')
+   with my_open(output_fpath) as file:
+      json.dump(output_data, file, indent=4)
              
 
 if __name__ == "__main__":
-    fire.Fire(main)
+   fire.Fire(main)
--- ChatGPT/get_embeddings.py
+++ ChatGPT/get_embeddings.py
@@ -12,79 +12,81 @@ sys.path.append("..")
 from utils import my_open
 
 def json_pretty_print(text, indent=4):
-    level = 0
-    list_level = 0
-    inside_apostrophe = 0
-    last_backslash_idx = -2
-    ret = ""
-    for i,c in enumerate(text):
-        if c=="}" and inside_apostrophe % 2 == 0:
-            level -= 1
-            ret += "\n" + " "*(level*indent)
-        ret += c
-        if c=="{" and inside_apostrophe % 2 == 0:
-            level += 1
-            ret += "\n" + " "*(level*indent)
-        elif c=="[" and inside_apostrophe % 2 == 0:
-            list_level += 1
-        elif c=="]" and inside_apostrophe % 2 == 0:
-            list_level -= 1
-        elif c=='"' and last_backslash_idx != i-1:
-            inside_apostrophe += 1
-        elif c=="\\":
-            last_backslash_idx=i
-        elif c=="," and inside_apostrophe % 2 == 0 and list_level<2:
-            ret += "\n" + " "*(level*indent)
-    return ret
+   level = 0
+   list_level = 0
+   inside_apostrophe = 0
+   last_backslash_idx = -2
+   ret = ""
+   for i,c in enumerate(text):
+      if c=="}" and inside_apostrophe % 2 == 0:
+         level -= 1
+         ret += "\n" + " "*(level*indent)
+      ret += c
+      if c=="{" and inside_apostrophe % 2 == 0:
+         level += 1
+         ret += "\n" + " "*(level*indent)
+      elif c=="[" and inside_apostrophe % 2 == 0:
+         list_level += 1
+      elif c=="]" and inside_apostrophe % 2 == 0:
+         list_level -= 1
+      elif c=='"' and last_backslash_idx != i-1:
+         inside_apostrophe += 1
+      elif c=="\\":
+         last_backslash_idx=i
+      elif c=="," and inside_apostrophe % 2 == 0 and list_level<2:
+         ret += "\n" + " "*(level*indent)
+   return ret
 
 def main(
-    prompt_file: str,
-    model_name: str="text-embedding-3-small",
-    output_path: str="model_inputs/IMDB",
-    dim: Union[None, int]=None,
-    **kwargs
+   prompt_file: str,
+   model_name: str="text-embedding-3-small",
+   output_path: str="model_inputs/IMDB",
+   dim: Union[None, int]=None,
+   **kwargs
 ):
-    if prompt_file is None:
-        print("No user prompt provided (option --promt-file). Exiting.")
-        sys.exit(1)
+   if len(kwargs) > 0:
+      raise ValueError(f"Unknown argument(s): {kwargs}")
+   if prompt_file is None:
+      print("No user prompt provided (option --promt-file). Exiting.")
+      sys.exit(1)
 
-    assert os.path.exists(prompt_file), f"Provided Prompt file does not exist {prompt_file}"
-    extension = os.path.splitext(prompt_file)[1].strip(".")
-    if extension.lower() in ["json"]:
-        with open(prompt_file, "r") as f:
-            user_prompts = json.load(f)
-        assert isinstance(user_prompts, list), "JSON content is not a list"
-    else:
-        assert False, f"Error: unrecognized Prompt file extension '{extension}'!"
+   assert os.path.exists(prompt_file), f"Provided Prompt file does not exist {prompt_file}"
+   extension = os.path.splitext(prompt_file)[1].strip(".")
+   if extension.lower() in ["json"]:
+      with open(prompt_file, "r") as f:
+         user_prompts = json.load(f)
+      assert isinstance(user_prompts, list), "JSON content is not a list"
+   else:
+      assert False, f"Error: unrecognized Prompt file extension '{extension}'!"
 
-    if dim is None:
-        get_emb = lambda entry: client.embeddings.create(input = [entry["txt"]], model=model_name).data[0].embedding
-    else:
-        get_emb = lambda entry: client.embeddings.create(input = [entry["txt"]], model=model_name, dimensions=dim).data[0].embedding
-    for entry in tqdm(user_prompts):
-        try:
+   if dim is None:
+      get_emb = lambda entry: client.embeddings.create(input = [entry["txt"]], model=model_name).data[0].embedding
+   else:
+      get_emb = lambda entry: client.embeddings.create(input = [entry["txt"]], model=model_name, dimensions=dim).data[0].embedding
+   for entry in tqdm(user_prompts):
+      try:
+         entry["embedding"] = get_emb(entry)
+      except Exception as error:
+         print(error)
+         print("Inference failed, will try again in a minute...")
+         time.sleep(60)
+         try:
             entry["embedding"] = get_emb(entry)
-        except Exception as error:
+         except Exception as error:
             print(error)
-            print("Inference failed, will try again in a minute...")
-            time.sleep(60)
+            print("Inference failed, will try again in a 10 minutes...")
+            time.sleep(600)
             try:
-                entry["embedding"] = get_emb(entry)
+               entry["embedding"] = get_emb(entry)
             except Exception as error:
-                print(error)
-                print("Inference failed, will try again in a 10 minutes...")
-                time.sleep(600)
-                try:
-                    entry["embedding"] = get_emb(entry)
-                except Exception as error:
-                    print(error)
-                    print(f"Inference failed again, terminating session...")
-                    break
-        
-    output_fpath = os.path.join(output_path,f'{os.path.splitext(os.path.basename(prompt_file))[0].strip(".")}_{os.path.basename(model_name.rstrip("/"))}_dim-{dim}.json')
-    with my_open(output_fpath) as outfile:
-        outfile.write(json_pretty_print(json.dumps(user_prompts,separators=(',', ': '))))
-        #json.dump(user_prompts, file, indent=4)
+               print(error)
+               print(f"Inference failed again, terminating session...")
+               break
+      
+   output_fpath = os.path.join(output_path,f'{os.path.splitext(os.path.basename(prompt_file))[0].strip(".")}_{os.path.basename(model_name.rstrip("/"))}_dim-{dim}.json')
+   with my_open(output_fpath) as outfile:
+      outfile.write(json_pretty_print(json.dumps(user_prompts,separators=(',', ': '))))
+      #json.dump(user_prompts, file, indent=4)
 
 if __name__ == "__main__":
-    fire.Fire(main)
+   fire.Fire(main)
