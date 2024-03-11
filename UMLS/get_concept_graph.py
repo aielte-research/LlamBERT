033f46add1b7bfac21ac8173e7a867a1faac836a
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2024-03-11 13:36:14 +0100

    .

--- UMLS/get_concept_graph.py
+++ UMLS/get_concept_graph.py
@@ -3,13 +3,12 @@ import os
 import json
 from tqdm import tqdm
 import fire
-import sys
 import networkx as nx
 
 def main(
    META_path: str="/home/projects/DeepNeurOntology/UMLS/2023AA/META/",
    concepts_fpath: str="regions/region_concepts_combined-microsoft-BiomedBERT_PubMed-filtered-counts.json",
-   output_fpath: str="regions/region_concepts_combined-microsoft-BiomedBERT_PubMed-filtered-counts.gexf",
+   output_fpath: str="regions/region_concepts_graph_combined-microsoft-BiomedBERT_PubMed-filtered.gexf",
    **kwargs
 ):
    if len(kwargs) > 0:
@@ -27,13 +26,13 @@ def main(
    MRREL = pd.read_csv(MRREL_fpath, sep="|", names=cols, dtype=dtypes, index_col=False)
    MRREL = MRREL[MRREL['CUI1'].isin(concepts.keys()) & MRREL['CUI2'].isin(concepts.keys())]
    MRREL = MRREL[MRREL["REL"] == 'CHD']
-   #MRREL = MRREL[MRREL["CUI1"] != MRREL["CUI2"]]
+   MRREL = MRREL[MRREL["CUI1"] != MRREL["CUI2"]]
    print(MRREL)
 
    G = nx.DiGraph()
+   G.add_nodes_from(concepts.keys())
    for _, row in tqdm(MRREL.iterrows(), total=len(MRREL)):
       G.add_edge(row['CUI1'], row['CUI2'])
-   print(list(G.nodes()))
 
    nx.write_gexf(G, output_fpath)
 
